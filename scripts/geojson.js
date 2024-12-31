9ba75ffa8acf75ffcb1adfb749d62ae351e18e4d
Adham125 <ahn508@york.ac.uk>
2024-12-31 21:12:44 +0200

    Update geojson.js

--- scripts/geojson.js
+++ scripts/geojson.js
@@ -1,4 +1,4 @@
-import { area, bbox, randomPoint, booleanPointInPolygon } from 'https://cdn.jsdelivr.net/npm/@turf/turf@6.5.0/dist/turf.min.js';
+import { area, bbox, randomPoint, booleanPointInPolygon } from 'https://cdn.jsdelivr.net/npm/@turf/turf@7.2.0/turf.min.js';
 
 export async function pickRandomPoint(geojson) {
   const features = geojson.features;
